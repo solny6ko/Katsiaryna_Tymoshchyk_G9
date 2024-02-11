845af228290435d5528801e13476b9f77f80ead7
Katsiaryna Tymoshchyk <solny6ko.net@gmail.com>
2024-02-11 21:40:47 +0200

    Day 4 Home task - arraya and console - part 2/4 - sumOfNelements

--- src/homework/day4/ArraysConsoleTask.java
+++ src/homework/day4/ArraysConsoleTask.java
@@ -14,6 +14,37 @@ public class ArraysConsoleTask {
 
     }
 
+//метод, который вернет целочисленный массив, состоящий только из тех элементов исходного целочисленного массива
+    //(подается на вход этого метода), которые больше, чем число n
+    // где n - целое число, передаваемое в сигнатуру этого метода,
+    //элементы в результирующем массиве должны быть расположены в обратном порядке
+
+    public void selectedElementsAreGraterThanNReverse(int elementNumber, int[] yourArray) {
+
+//        считаем количество элементов которые больше elementNumber
+        int sum = 0;
+        for (int i = 0; i < yourArray.length; i++) {
+            if (yourArray[i] > elementNumber) {
+                sum += 1;
+            }
+        }
+//          создаем массив длины sum
+        int[] resultArray = new int[sum];
+
+//Начиная с конца исходного массива проверяем, какие элементы исходного массива больше указанного числа elementNumber
+// и записываем их в новый массив
+        System.out.println("Elements in reverse order which are grater than " + elementNumber + ": ");
+        int resultArrayIndex = 0;
+        for (int i = yourArray.length - 1; i >= 0; i--) {
+            if (yourArray[i] > elementNumber) {
+                resultArray[resultArrayIndex] = yourArray[i];
+                System.out.print(resultArray[resultArrayIndex] + " ");
+                resultArrayIndex++;
+            }
+        }
+        System.out.println("");
+
+    }
 
 
 }
--- src/homework/day4/ArraysConsoleTaskRunner.java
+++ src/homework/day4/ArraysConsoleTaskRunner.java
@@ -5,6 +5,7 @@ public class ArraysConsoleTaskRunner {
         int[] yourArray = {1,4,6,33,30,90,44,86,90,46,74};
         ArraysConsoleTask arraysConsoleTask = new ArraysConsoleTask();
         arraysConsoleTask.sumOfNelements(5,yourArray);
+        arraysConsoleTask.selectedElementsAreGraterThanNReverse(40,yourArray);
 
 
     }
